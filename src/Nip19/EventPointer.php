12adada51f262cd27b95dc31bf5083a74ffe9509
kawax <kawaxbiz@gmail.com>
2023-03-30 18:49:38 +0900

    Update EventPointer.php

--- src/Nip19/EventPointer.php
+++ src/Nip19/EventPointer.php
@@ -11,12 +11,14 @@ class EventPointer implements Arrayable
     public function __construct(
         protected readonly string $id,
         protected readonly array $relays = [],
+        protected readonly string $author = '',
     ) {
     }
 
     public static function make(
         string $id,
         array $relays = [],
+        string $author = '',
     ): static {
         return new static(...func_get_args());
     }
