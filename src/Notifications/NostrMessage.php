0b4aa64a9bc4b7805c59549e60a82477ed6d2847
kawax <kawaxbiz@gmail.com>
2024-04-18 08:21:54 +0900

    Update NostrMessage.php

--- src/Notifications/NostrMessage.php
+++ src/Notifications/NostrMessage.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace Revolution\Nostr\Notifications;
 
 use Illuminate\Contracts\Support\Arrayable;
+use Revolution\Nostr\Kind;
 
 class NostrMessage implements Arrayable
 {
     public function __construct(
-        public readonly string $content,
-        public readonly int $kind = 1,
-        public readonly array $tags = [],
-    ) {
+        public readonly string   $content,
+        public readonly int|Kind $kind = Kind::Text,
+        public readonly array    $tags = [],
+    )
+    {
+        //
     }
 
-    public static function create(string $content, int $kind = 1, array $tags = []): static
+    public static function create(string $content, int|Kind $kind = Kind::Text, array $tags = []): static
     {
         return new static(content: $content, kind: $kind, tags: $tags);
     }
