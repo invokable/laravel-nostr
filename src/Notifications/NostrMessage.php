a99acbd017ff6dd5ea972174372eaf10304ae833
kawax <kawaxbiz@gmail.com>
2024-09-16 17:40:44 +0900

    Update NostrMessage.php

--- src/Notifications/NostrMessage.php
+++ src/Notifications/NostrMessage.php
@@ -7,19 +7,19 @@ namespace Revolution\Nostr\Notifications;
 use Illuminate\Contracts\Support\Arrayable;
 use Revolution\Nostr\Kind;
 
-class NostrMessage implements Arrayable
+final readonly class NostrMessage implements Arrayable
 {
     public function __construct(
-        public readonly string $content,
-        public readonly int|Kind $kind = Kind::Text,
-        public readonly array $tags = [],
+        public string $content,
+        public int|Kind $kind = Kind::Text,
+        public array $tags = [],
     ) {
         //
     }
 
-    public static function create(string $content, int|Kind $kind = Kind::Text, array $tags = []): static
+    public static function create(string $content, int|Kind $kind = Kind::Text, array $tags = []): self
     {
-        return new static(...func_get_args());
+        return new self(...func_get_args());
     }
 
     public function toArray(): array
