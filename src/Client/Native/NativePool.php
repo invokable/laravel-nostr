9d61e4c88db417bfd4829f6fd4e9726a5102bd5e
kawax <kawaxbiz@gmail.com>
2024-10-28 21:13:00 +0900

    Update NativePool.php

--- src/Client/Native/NativePool.php
+++ src/Client/Native/NativePool.php
@@ -11,11 +11,13 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
 use Revolution\Nostr\Client\Native\Concerns\HasEvent;
-use Revolution\Nostr\Client\Native\Concerns\HasFilter;
 use Revolution\Nostr\Client\Native\Concerns\HasHttp;
 use Revolution\Nostr\Contracts\Client\ClientPool;
 use Revolution\Nostr\Event;
 use Revolution\Nostr\Filter;
+use swentel\nostr\RelayResponse\RelayResponse;
+use swentel\nostr\RelayResponse\RelayResponseNotice;
+use swentel\nostr\RelayResponse\RelayResponseOk;
 
 /**
  * Working with multiple relays.
@@ -24,7 +26,6 @@ class NativePool implements ClientPool
 {
     use HasHttp;
     use HasEvent;
-    use HasFilter;
     use Macroable;
     use Conditionable;
 
@@ -49,12 +50,34 @@ class NativePool implements ClientPool
     {
         $relays = blank($relays) ? $this->relays : $relays;
 
-        return Http::pool(fn (Pool $pool) => collect($relays)
+        $responses = Http::pool(fn (Pool $pool) => collect($relays)
             ->map(fn ($relay) => $pool->as($relay)->ws($relay, function (NativeWebSocket $ws) use ($event, $sk) {
                 return $this->response($ws->publish($event, $sk));
             }))
             ->toArray(),
         );
+
+        return collect($responses)
+            ->map(function ($response) {
+                $res = RelayResponse::create($response->json() ?? ['NOTICE', 'error']);
+
+                if ($res instanceof RelayResponseNotice) {
+                    return $this->response([
+                        'message' => 'error',
+                        'type' => $res->type,
+                        'error' => $res->message(),
+                    ], 500);
+                }
+
+                if ($res instanceof RelayResponseOk && $res->isSuccess()) {
+                    return $this->response([
+                        'message' => $res->type,
+                        'id' => $res->eventId,
+                    ]);
+                }
+
+                return $this->response(['message' => 'error', 'error' => 'error'], 500);
+            })->toArray();
     }
 
     /**
