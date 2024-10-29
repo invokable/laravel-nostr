88ac3bb227ee8831756c92e4994ab9c90234bc12
kawax <kawaxbiz@gmail.com>
2024-10-29 13:48:40 +0900

    Update ClientRelayTest.php

--- tests/Feature/Client/Native/ClientRelayTest.php
+++ tests/Feature/Client/Native/ClientRelayTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Client\Native;
 
+use Illuminate\Support\Facades\Http;
 use Revolution\Nostr\Facades\Nostr;
 use Tests\TestCase;
 
@@ -11,6 +12,10 @@ class ClientRelayTest extends TestCase
 {
     public function test_info()
     {
+        if (filled(getenv('GITHUB_TOKEN'))) {
+            Http::fake(fn () => Http::response(['name' => 'test']));
+        }
+
         $response = Nostr::relay()->info(relays: config('nostr.relays'));
 
         $errors = collect($response)->whereNull('name')->toArray();
