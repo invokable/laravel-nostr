03605d7e3ba52befc0e106c8a6c9e521aae6fc21
kawax <kawaxbiz@gmail.com>
2025-06-06 11:31:14 +0900

    Update ClientNip19Test.php

--- tests/Feature/Client/Native/ClientNip19Test.php
+++ tests/Feature/Client/Native/ClientNip19Test.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Tests\Feature\Client\Native;
 
 use Illuminate\Http\Client\Response;
-use Revolution\Nostr\Client\Native\NativeNip19;
+use Revolution\Nostr\Facades\Nostr;
 use Revolution\Nostr\Nip19\AddressPointer;
 use Revolution\Nostr\Nip19\EventPointer;
 use Revolution\Nostr\Nip19\ProfilePointer;
@@ -15,17 +15,16 @@ class ClientNip19Test extends TestCase
 {
     public function test_decode()
     {
-        $client = new NativeNip19;
-        $response = $client->decode('npub1qe3e5wrvnsgpggtkytxteaqfprz0rgxr8c3l34kk3a9t7e2l3acslezefe');
+        $response = Nostr::native()->nip19()->decode('npub1qe3e5wrvnsgpggtkytxteaqfprz0rgxr8c3l34kk3a9t7e2l3acslezefe');
 
+        $response->dump();
         $this->assertInstanceOf(Response::class, $response);
         $this->assertTrue($response->successful());
     }
 
     public function test_note()
     {
-        $client = new NativeNip19;
-        $response = $client->note('43fb0422457c1fadec68c5ad18378abb2c626d6b787790973e888d0998f6ced4');
+        $response = Nostr::native()->nip19()->note('43fb0422457c1fadec68c5ad18378abb2c626d6b787790973e888d0998f6ced4');
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertTrue($response->successful());
@@ -35,8 +34,7 @@ class ClientNip19Test extends TestCase
     {
         $profile = ProfilePointer::make('06639a386c9c1014217622ccbcf40908c4f1a0c33e23f8d6d68f4abf655f8f71', ['wss://relay.example.com']);
 
-        $client = new NativeNip19;
-        $response = $client->nprofile($profile);
+        $response = Nostr::native()->nip19()->nprofile($profile);
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertTrue($response->successful());
@@ -46,8 +44,7 @@ class ClientNip19Test extends TestCase
     {
         $event = EventPointer::make('43fb0422457c1fadec68c5ad18378abb2c626d6b787790973e888d0998f6ced4', ['wss://relay.example.com'], '06639a386c9c1014217622ccbcf40908c4f1a0c33e23f8d6d68f4abf655f8f71');
 
-        $client = new NativeNip19;
-        $response = $client->nevent($event);
+        $response = Nostr::native()->nip19()->nevent($event);
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertTrue($response->successful());
@@ -57,8 +54,7 @@ class ClientNip19Test extends TestCase
     {
         $addr = AddressPointer::make('test_identifier', '06639a386c9c1014217622ccbcf40908c4f1a0c33e23f8d6d68f4abf655f8f71', 30023, ['wss://relay.example.com']);
 
-        $client = new NativeNip19;
-        $response = $client->naddr($addr);
+        $response = Nostr::native()->nip19()->naddr($addr);
 
         $this->assertInstanceOf(Response::class, $response);
         $this->assertTrue($response->successful());
