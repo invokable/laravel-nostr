63b0edb2846daef0b900712c5a2577b92588fa82
kawax <kawaxbiz@gmail.com>
2025-06-06 10:22:24 +0900

    Create NativeNip19.php

---
+++ src/Client/Native/NativeNip19.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Revolution\Nostr\Client\Native;
+
+use BitWasp\Bech32\Exception\Bech32Exception;
+use Exception;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
+use Revolution\Nostr\Client\Native\Concerns\HasHttp;
+use swentel\nostr\Nip19\Nip19Helper;
+
+class NativeNip19
+{
+    use Conditionable;
+    use HasHttp;
+    use Macroable;
+
+    /**
+     * Decode NIP-19 string.
+     *
+     * @param  string  $n  nsec, npub, note, nprofile, nevent, naddr
+     *
+     * @throws Exception
+     */
+    public function decode(string $n): Response
+    {
+        $nip19 = new Nip19Helper;
+
+        return $this->response($nip19->decode($n));
+    }
+
+    /**
+     * encode note id.
+     *
+     * @throws Bech32Exception
+     */
+    public function note(string $id): Response
+    {
+        $nip19 = new Nip19Helper;
+
+        return $this->response($nip19->encodeNote($id));
+    }
+}
