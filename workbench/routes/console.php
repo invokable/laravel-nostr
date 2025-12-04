8361910f2af7e98bcc4181e0b463137dbbebd1b4
kawax <kawaxbiz@gmail.com>
2025-12-04 19:46:19 +0900

    Add Nostr key generation and event list commands
    Introduced two new Artisan commands: 'nostr:keys' for generating Nostr keys and 'nostr:list' for listing recent text events using the Nostr package. These commands enhance console functionality for Nostr integration and testing.

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Revolution\Nostr\Facades\Nostr;
+use Revolution\Nostr\Filter;
+use Revolution\Nostr\Kind;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
+
+// vendor/bin/testbench nostr:keys
+Artisan::command('nostr:keys', function () {
+    $response = Nostr::key()->generate();
+    $this->info($response->collect()->toPrettyJson());
+});
+
+// vendor/bin/testbench nostr:list
+Artisan::command('nostr:list', function () {
+    $response = Nostr::event()->list(Filter::make(kinds: [Kind::Text], limit: 5));
+    $this->info($response->collect()->toPrettyJson());
+});
