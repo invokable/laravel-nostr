e52939a3823815ead62d0f18e6bf246be202f0e1
kawax <kawaxbiz@gmail.com>
2025-12-25 15:15:28 +0900

    Update Nostr relays and add relay info command
    Removed several relays from the Nostr configuration and commented out two others. Added a new Artisan command 'nostr:info' to check the reachability and information of configured Nostr relays.

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Revolution\Nostr\Facades\Nostr;
 use Revolution\Nostr\Filter;
 use Revolution\Nostr\Kind;
+use Illuminate\Support\Arr;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
@@ -21,3 +22,15 @@ Artisan::command('nostr:list', function () {
     $response = Nostr::event()->list(Filter::make(kinds: [Kind::Text], limit: 5));
     $this->info($response->collect()->toPrettyJson(JSON_UNESCAPED_UNICODE));
 });
+
+// vendor/bin/testbench nostr:info
+Artisan::command('nostr:info', function () {
+    $responses = Nostr::relay()->info(config('nostr.relays'));
+    foreach ($responses as $relay => $response) {
+        if (Arr::exists($response, 'error')) {
+            $this->error("Relay: $relay Error: {$response['error']}");
+        } else {
+            // dump($relay, $response);
+        }
+    }
+})->purpose('Ensure Nostr relays are reachable and display their information.');
